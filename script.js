212dbe9f9d4a0bfcf7e60df5f4530448a41aba44
Tony Huynh <huynh.tony55@gmail.com>
2019-11-22 16:54:38 +1000

    implemented math logic to display current time

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
-SECHAND.style.transform = "rotate(" + secPosition + "deg)"
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
